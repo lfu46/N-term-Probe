ae160b65ec2f0c7eed403a0ec7e846cfcb6977a9
Longping Fu <lpfu46@gatech.edu>
2025-02-12 07:06:37 -0500

    add raw data

---
+++ data_analysis/data_import.R
@@ -0,0 +1,7 @@
+#import packages
+packages_names <- c("tidyverse", "readxl", "writexl")
+lapply(packages_names, require, character.only = TRUE)
+
+#import degradation data from MSFragger
+#HEK_Nt_1
+
