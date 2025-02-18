9fc41ba4fac7592be549ba26ea6a785d6c882d87
Longping Fu <lpfu46@gatech.edu>
2025-02-18 06:50:32 -0500

    reanalysis topfinder result

--- data_analysis/Nterm_topfinder.R
+++ data_analysis/Nterm_topfinder.R
@@ -16,7 +16,7 @@ write_delim(topfinder_id,
 
 # import result from TopFinder
 Nterm_topfinder_result <- read_delim(
-  'data_source/Nterm_topfinder/2025_02_15_Nterm_02152025/2025_02_15_Nterm_02152025_Full_Table.txt',
+  'data_source/Nterm_topfinder/2025_02_17_Nterm_02172025/2025_02_17_Nterm_02172025_Full_Table.txt',
   col_names = TRUE,
   name_repair = 'universal'
 ) |> 
@@ -26,20 +26,27 @@ Nterm_topfinder_result <- read_delim(
 # Nterm cleaving proteases
 Nterm_topfinder_cleaving_proteases <- Nterm_topfinder_result |> 
   select(Index, Cleaving.proteases) |> 
-  left_join(HEK_Nterm_Kd_half_life, by = 'Index') |> 
+  left_join(HEK_Nterm_Kd_half_life_LaminB_Tcomplex, by = 'Index') |> 
   filter(!is.na(Cleaving.proteases)) |> 
   separate_rows(Cleaving.proteases, sep = ';')
 
 # Wilcoxon rank-sum test
-Nterm_topfinder_cleaving_proteases |> 
-  wilcox_test(half_life ~ Cleaving.proteases) |> 
+Cleaving.Proteases.List <- Nterm_topfinder_cleaving_proteases |> 
+  group_by(Cleaving.proteases) |> 
+  get_summary_stats(half_life, type = 'median') |> 
+  filter(n > 1) |> 
+  pull(Cleaving.proteases)
+
+Nterm_topfinder_cleaving_proteases |>
+  filter(Cleaving.proteases %in% Cleaving.Proteases.List) |> 
+  wilcox_test(half_life ~ Cleaving.proteases) |>
   filter(p < 0.05)
 
 # Nterm signal peptide
 Nterm_topfinder_signal_peptide <- Nterm_topfinder_result |> 
   select(Index, signal = Distance.To.signal.peptide) |> 
   filter(!is.na(signal)) |> 
-  left_join(HEK_Nterm_Kd_half_life, by = 'Index') |> 
+  left_join(HEK_Nterm_Kd_half_life_LaminB_Tcomplex, by = 'Index') |> 
   mutate(
     signal_type = ifelse(signal >= -10 & signal <= 10, 'signal peptide', 'non signal peptide')
   )
@@ -52,7 +59,7 @@ Nterm_topfinder_signal_peptide |>
 Nterm_topfinder_propeptide <- Nterm_topfinder_result |> 
   select(Index, propeptide = Distance.to.propeptide.lost) |> 
   filter(!is.na(propeptide)) |> 
-  left_join(HEK_Nterm_Kd_half_life, by = 'Index') |> 
+  left_join(HEK_Nterm_Kd_half_life_LaminB_Tcomplex, by = 'Index') |> 
   mutate(
     propeptide_type = ifelse(propeptide >= -10 & propeptide <= 10, 'propeptide', 'non propeptide')
   )
@@ -65,7 +72,7 @@ Nterm_topfinder_propeptide |>
 Nterm_topfinder_transmembrane <- Nterm_topfinder_result |> 
   select(Index, transmembrane = Distance.to.last.transmembrane.domain..shed.) |> 
   filter(!is.na(transmembrane)) |> 
-  left_join(HEK_Nterm_Kd_half_life, by = 'Index') |> 
+  left_join(HEK_Nterm_Kd_half_life_LaminB_Tcomplex, by = 'Index') |> 
   mutate(
     transmembrane_type = ifelse(transmembrane >= -10 & transmembrane <= 10, 'transmembrane', 'non transmembrane')
   )
