d6333a86e00d9db21e2830bf069101c922c40214
Longping Fu <lpfu46@gatech.edu>
2025-02-22 20:56:18 -0500

    perform Nterm structure analysis

--- data_analysis/Nterm_structuremap.R
+++ data_analysis/Nterm_structuremap.R
@@ -11,3 +11,37 @@ use_condaenv(
 
 # execute the python script for Nterm structure
 source_python("data_analysis/Nterm_structuremap.py")
+
+# combine Nterm AlphaFold information with Nterm half life result
+Nterm_alphafold_half_life <- tibble(Nterm_alphafold_accessibility_smooth) |> 
+  mutate(
+    Index = str_c(protein_id, position, sep = '_')
+  ) |> 
+  select(
+    Index, UniProt_Accession = protein_id, secondary_structure:IDR
+  ) |> 
+  filter(Index %in% HEK_Nterm_Kd_half_life_sequence$Index) |> 
+  left_join(HEK_Nterm_Kd_half_life_sequence, by = c('Index', 'UniProt_Accession'))
+
+# Wilcoxon rank-sum test
+library(rstatix)
+
+# secondary_structure
+Nterm_alphafold_half_life |> 
+  wilcox_test(half_life ~ secondary_structure) |> 
+  filter(p < 0.05)
+
+# structure_group
+Nterm_alphafold_half_life |> 
+  wilcox_test(half_life ~ structure_group) |> 
+  filter(p < 0.05)
+
+# accessibility
+Nterm_alphafold_half_life |> 
+  wilcox_test(half_life ~ accessibility) |> 
+  filter(p < 0.05)
+
+# IDR
+Nterm_alphafold_half_life |> 
+  wilcox_test(half_life ~ IDR) |> 
+  filter(p < 0.05)
--- data_analysis/Nterm_structuremap.py
+++ data_analysis/Nterm_structuremap.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import numpy as np
 
-# import strucutremap functions as instructed by tutorial (https://github.com/MannLabs/structuremap/blob/main/nbs/tutorial.ipynb)
+# import strucutremap functions as instructed by tutorial 
+# (https://github.com/MannLabs/structuremap/blob/main/nbs/tutorial.ipynb)
+# original paper ()
 import structuremap.utils
 structuremap.utils.set_logger()
 from structuremap.processing import download_alphafold_cif, download_alphafold_pae, format_alphafold_data, annotate_accessibility, get_smooth_score, annotate_proteins_with_idr_pattern, get_extended_flexible_pattern, get_proximity_pvals, perform_enrichment_analysis, perform_enrichment_analysis_per_protein, evaluate_ptm_colocalization, extract_motifs_in_proteome
@@ -30,8 +32,49 @@ Nterm_protein_list = HEK_Nterm_Kd_half_life_sequence["UniProt_Accession"].unique
 # )
 
 # format AlphaFold data input
-alphafold_annotation = format_alphafold_data(
+Nterm_alphafold_annotation = format_alphafold_data(
   directory = "data_source/Nterm_structuremap/Nterm_degradation_cif",
   protein_ids = Nterm_protein_list
 )
 
+# annotate prediction-aware part-sphere exposure (pPSE) values
+Nterm_full_sphere_exposure = annotate_accessibility(
+    df = Nterm_alphafold_annotation, 
+    max_dist = 24, 
+    max_angle = 180, 
+    error_dir = "data_source/Nterm_structuremap/Nterm_degradation_pae"
+)
+
+Nterm_alphafold_accessibility = Nterm_alphafold_annotation.merge(
+  Nterm_full_sphere_exposure,
+  how = "left",
+  on = ["protein_id", "AA", "position"]
+)
+
+Nterm_part_sphere_exposure = annotate_accessibility(
+    df = Nterm_alphafold_annotation, 
+    max_dist = 12, 
+    max_angle = 70, 
+    error_dir = "data_source/Nterm_structuremap/Nterm_degradation_pae"
+)
+
+Nterm_alphafold_accessibility = Nterm_alphafold_accessibility.merge(
+  Nterm_part_sphere_exposure,
+  how = "left",
+  on = ["protein_id", "AA", "position"]
+)
+
+Nterm_alphafold_accessibility["accessibility"] = np.where(
+  Nterm_alphafold_accessibility.nAA_12_70_pae <= 5, "high exposure", "low exposure"
+)
+
+# annotate intrisicly disorder region (IDR)
+Nterm_alphafold_accessibility_smooth = get_smooth_score(
+  Nterm_alphafold_accessibility,
+  np.array(["nAA_24_180_pae"]),
+  [10]
+).reset_index(drop=True)
+
+Nterm_alphafold_accessibility_smooth["IDR"] = np.where(
+  Nterm_alphafold_accessibility_smooth.nAA_24_180_pae_smooth10 <= 34.27, "IDR", "Structural Region"
+)
