1bebc0e7d3729e0687f497151f41c1c062b8d8af
Longping Fu <lpfu46@gatech.edu>
2025-02-24 21:04:34 -0500

    perform part of ELM related analysis

--- data_analysis/Nterm_sequence_feature_ELM_motif.R
+++ data_analysis/Nterm_sequence_feature_ELM_motif.R
@@ -1,6 +1,5 @@
 # import packages
-packages_names <- c("tidyverse", 'rstatix', 'clusterProfiler', 'org.Hs.eg.db')
-lapply(packages_names, require, character.only = TRUE)
+library(tidyverse)
 
 # import ELM classes downloaded from 
 # The Eukaryotic Linear Motif resources for Functional Sites in Proteins (http://elm.eu.org/searchdb.html)
@@ -27,9 +26,37 @@ HEK_Nterm_sequence_ELM_motif <- HEK_Nterm_Kd_half_life_sequence |>
   distinct()
 
 # calculate medain half life for each ELM motif
+library(rstatix)
+
 HEK_Nterm_sequence_ELM_motif_median <- HEK_Nterm_sequence_ELM_motif |> 
   group_by(matched_motifs) |> 
   get_summary_stats(half_life, type = 'median') |> 
   arrange(median)
 
 write_csv(HEK_Nterm_sequence_ELM_motif_median, file = 'data_source/ELM_degron/HEK_Nterm_sequence_ELM_motif_median.csv')
+
+## GSEA for ELM motif
+# construct database
+ELM_motif_database <- HEK_Nterm_sequence_ELM_motif |> 
+  select(matched_motifs, Index) |> 
+  distinct()
+
+# genelist descending Kd
+geneList_des_Kd <- HEK_Nterm_Kd_half_life_sequence$Kd
+names(geneList_des_Kd) <- HEK_Nterm_Kd_half_life_sequence$Index
+geneList_des_Kd <- sort(geneList_des_Kd, decreasing = TRUE)
+
+# GSEA
+library(clusterProfiler)
+
+Nterm_ELM_motif_GSEA_des_Kd <- GSEA(
+  geneList = geneList_des_Kd,
+  TERM2GENE = ELM_motif_database,
+  pvalueCutoff = 1,
+  scoreType = 'pos'
+)
+
+write_csv(
+  Nterm_ELM_motif_GSEA_des_Kd@result,
+  'data_source/ELM_degron/Nterm_ELM_motif_GSEA_des_Kd.csv'
+)
--- data_analysis/figure6.R
+++ data_analysis/figure6.R
@@ -1,68 +1,255 @@
 # import packages
-packages_names <- c('tidyverse', 'rstatix', 'ggpubr', 'showtext', 'ComplexHeatmap', 'circlize')
-lapply(packages_names, require, character.only = TRUE)
+library(tidyverse)
 
-### figure 5A, ELM N-degrons
+### figure 6A, ELM N-degrons
 # Wilcoxon rank-sum test
+library(rstatix)
+
 HEK_Nterm_ELM_N_degron_wilcoxon_test <- HEK_Nterm_ELM_N_degron |>
   wilcox_test(half_life ~ ELM_N_degron) |> 
   add_significance('p') |> 
-  filter(p < 0.01)
+  filter(p < 0.05)
 
-# boxplot
-font_add(family = 'arial', regular = 'arial.ttf')
-showtext_auto()
+# point range plot
+library(ggpubr)
 
-boxplot_Nterm_degron <- HEK_Nterm_ELM_N_degron |> 
+point_range_Nterm_degron <- HEK_Nterm_ELM_N_degron |> 
   ggplot() +
-  geom_boxplot(
+  geom_point(
     aes(
       x = fct_reorder(ELM_N_degron, half_life),
       y = half_life
     ),
-    color = color_1, outliers = FALSE
+    position = position_jitter(width = 0.3),
+    color = 'black',
+    alpha = 0.1,
+    size = 0.5
+  ) +
+  stat_summary(
+    aes(
+      x = fct_reorder(ELM_N_degron, half_life),
+      y = half_life 
+    ),
+    fun.data = 'mean_cl_boot', color = color_1, linewidth = 0.2, size = 0.5
   ) +
   stat_pvalue_manual(
     data = HEK_Nterm_ELM_N_degron_wilcoxon_test, label = 'p.signif', label.size = 6, 
-    tip.length = 0, y.position = c(23, 26, 29)
+    tip.length = 0, y.position = c(160, 180)
   ) +
   labs(x = '', y = '') +
-  coord_cartesian(ylim = c(0, 30)) +
-  theme_bw() +
   theme(
-    panel.grid.major = element_line(color = 'gray', linewidth = 0.2),
-    panel.grid.minor = element_line(color = 'gray', linewidth = 0.1),
     axis.text.x = element_text(color = 'black', size = 9, angle = 30, hjust = 1),
     axis.text.y = element_text(color = 'black', size = 9)
   )
 
 ggsave(
-  filename = 'figures/figure5/boxplot_Nterm_degron.eps',
-  plot = boxplot_Nterm_degron,
-  height = 2, width = 2.5, units = 'in'
+  filename = 'figures/figure5/point_range_Nterm_degron.eps',
+  plot = point_range_Nterm_degron,
+  device = cairo_ps,
+  height = 2, width = 2.5, units = 'in',
+  fallback_resolution = 1200
 )
 
-### figure 5B, ELM motifs
-# Wilcoxon rank-sum test
-HEK_Nterm_sequence_ELM_motif_wilcoxon_test <- HEK_Nterm_sequence_ELM_motif |> 
-  filter(
-    matched_motifs %in% c(
-      'APC/C_Apc2-docking motif', 
-      'LEDGF/P75 IBD binding site', 
-      'Trans-Golgi Network-Endosome-Lysosome-sorting signals',
-      'Corepressor nuclear receptor box',
-      'FBP Nbox motif',
-      'Gamma-adaptin ear interaction motif',
-      'KLHDC2 C-terminal GG degrons',
-      'CRL4-Cdt2 binding PIP degron',
-      'CASK CaMK domain binding ligand motif',
-      'DCAF12 C-terminal diGlu degrons'
-    )
+### figure 6B, ELM motifs
+# import GSEA result from ELM motif analysis
+Nterm_ELM_motif_GSEA_des_Kd <- read_csv(
+  'data_source/ELM_degron/Nterm_ELM_motif_GSEA_des_Kd.csv'
+)
+
+# filter the enriched term which p < 0.05, rearrange the terms and export as a table
+library(gridExtra)
+
+Nterm_ELM_motif_GSEA_des_Kd_enriched_term <- Nterm_ELM_motif_GSEA_des_Kd |> 
+  filter(pvalue < 0.05) |> 
+  select(
+    'ELM.Name' = Description, NES, pvalue
   ) |> 
+  mutate(
+    NES = round(NES, 2) , 
+    'P.value' = format(pvalue, format = "e", digits = 2)
+  ) |> 
+  select(-pvalue) |> 
+  arrange(desc(NES))
+
+grid.table(Nterm_ELM_motif_GSEA_des_Kd_enriched_term)
+
+# generate proteoform list for related motif
+PIP_degron_list <- Nterm_ELM_motif_GSEA_des_Kd |> 
+  filter(Description == 'CRL4-Cdt2 binding PIP degron') |> 
+  select(core_enrichment) |> 
+  separate_rows(core_enrichment, sep = '/') |> 
+  pull(core_enrichment)
+
+CASK_binding_list <- Nterm_ELM_motif_GSEA_des_Kd |> 
+  filter(Description == 'CASK CaMK domain binding ligand motif') |> 
+  select(core_enrichment) |> 
+  separate_rows(core_enrichment, sep = '/') |> 
+  pull(core_enrichment)
+
+AGC_docking_list <- Nterm_ELM_motif_GSEA_des_Kd |> 
+  filter(Description == 'AGC Kinase docking motif') |> 
+  select(core_enrichment) |> 
+  separate_rows(core_enrichment, sep = '/') |> 
+  pull(core_enrichment)
+
+HCF_binding_list <- Nterm_ELM_motif_GSEA_des_Kd |> 
+  filter(Description == 'HCF-1 binding motif') |> 
+  select(core_enrichment) |> 
+  separate_rows(core_enrichment, sep = '/') |> 
+  pull(core_enrichment)
+
+# Nterm proteoforms ranking plot
+Nterm_Kd_rank <- HEK_Nterm_Kd_half_life_sequence |> 
+  arrange(desc(Kd)) |> 
+  select(Index, Kd) |> 
+  mutate(ranking = rank(Kd))
+
+ranking_plot_Nterm_Kd <- ggplot() +
+  # for all the proteoforms that are not related to selected enriched motif
+  geom_point(
+    data = Nterm_Kd_rank |> filter(
+      ! Index %in% PIP_degron_list,
+      ! Index %in% CASK_binding_list,
+      ! Index %in% AGC_docking_list,
+      ! Index %in% HCF_binding_list
+    ),
+    aes(
+      x = ranking,
+      y = Kd
+    ),
+    shape = 21, color = 'gray50', fill = 'transparent'
+  ) +
+  # Nterm proteoforms related to CRL4-Cdt2 binding PIP degron
+  geom_point(
+    data = Nterm_Kd_rank |> filter(Index %in% PIP_degron_list),
+    aes(
+      x = ranking,
+      y = Kd
+    ),
+    shape = 21, color = 'black', fill = color_1, size = 3
+  ) +
+  # Nterm proteoforms related to CASK CaMK domain binding ligand motif
+  geom_point(
+    data = Nterm_Kd_rank |> filter(Index %in% CASK_binding_list),
+    aes(
+      x = ranking,
+      y = Kd
+    ),
+    shape = 21, color = 'black', fill = color_2, size = 3
+  ) +
+  # Nterm proteoforms related to AGC Kinase docking motif
+  geom_point(
+    data = Nterm_Kd_rank |> filter(Index %in% AGC_docking_list),
+    aes(
+      x = ranking,
+      y = Kd
+    ),
+    shape = 21, color = 'black', fill = color_3, size = 3
+  ) +
+  # Nterm proteoforms related to HCF-1 binding motif
+  geom_point(
+    data = Nterm_Kd_rank |> filter(Index %in% HCF_binding_list),
+    aes(
+      x = ranking,
+      y = Kd
+    ),
+    shape = 21, color = 'black', fill = color_4, size = 3
+  ) +
+  labs(x = '', y = '') +
+  theme(
+    axis.text = element_text(color = 'black', family = 'arial', size = 9)
+  )
+
+ggsave(
+  filename = 'figures/figure5/ranking_plot_Nterm_Kd.eps',
+  plot = ranking_plot_Nterm_Kd,
+  height = 1.5, width = 3, units = 'in'
+)
+
+## protein example plot
+# without motif
+protein_example_data <- tribble(
+  ~ start, ~ end, ~ top, ~ bottom,
+  0, 1000, 1, 2
+)
+
+protein_example_without_motif <- ggplot() +
+  geom_rect(
+    data = protein_example_data,
+    aes(
+      xmin = start, xmax = end, ymin = bottom, ymax = top
+    ),
+    fill = "gray80", color = "black"
+  ) +
+  theme_void() +
+  theme(
+    axis.text = element_blank()
+  )
+
+ggsave(
+  filename = 'figures/figure5/protein_example_without_motif.eps',
+  plot = protein_example_without_motif,
+  height = 1, width = 4, units = 'in'
+)
+
+# with motif
+protein_example_data <- tribble(
+  ~ start, ~ end, ~ top, ~ bottom,
+  0, 1000, 1, 2
+)
+
+motif_data <- tribble(
+  ~ motif, ~ start_position, ~ end_position, 
+  'motif_1', 100, 300,
+  'motif_2', 600, 900
+)
+
+protein_example_with_motif <- ggplot() +
+  geom_rect(
+    data = protein_example_data,
+    aes(
+      xmin = start, xmax = end, ymin = bottom, ymax = top
+    ),
+    fill = "gray80", color = "black"
+  ) +
+  geom_rect(
+    data = motif_data, 
+    aes(
+      xmin = start_position, 
+      xmax = end_position, 
+      ymin = protein_example_data$bottom, 
+      ymax = protein_example_data$top, 
+      fill = motif
+    ), 
+    show.legend = FALSE, color = 'transparent'
+  ) +
+  scale_fill_manual(
+    values = c(
+      "motif_1" = color_1,
+      "motif_2" = color_2
+    )
+  ) +
+  theme_void() +
+  theme(
+    axis.text = element_blank()
+  )
+
+ggsave(
+  filename = 'figures/figure5/protein_example_with_motif.eps',
+  plot = protein_example_with_motif,
+  height = 1, width = 4, units = 'in'
+)
+
+### figure 6C, degron-related ELM motif
+# Wilcoxon rank-sum test
+library(rstatix)
+
+HEK_Nterm_sequence_ELM_motif_wilcoxon_test <- HEK_Nterm_sequence_ELM_motif |> 
+  filter(str_detect(matched_motifs, 'degron')) |> 
+  filter(matched_motifs != 'N-degron') |> 
   wilcox_test(half_life ~ matched_motifs) |> 
   add_significance('p') |> 
-  filter(p < 0.05) |> 
-  slice(7, 13, 16)
+  filter(p < 0.05)
 
 # boxplot
 font_add(family = 'arial', regular = 'arial.ttf')
