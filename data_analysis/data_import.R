51c59c2eb672a86d52e7cd00e4373c04aa5768a2
Longping Fu <lpfu46@gatech.edu>
2025-03-04 07:03:01 -0500

    import whole proteome result

--- data_analysis/data_import.R
+++ data_analysis/data_import.R
@@ -1,9 +1,9 @@
-#import packages
-packages_names <- c("tidyverse", "readxl", "writexl")
-lapply(packages_names, require, character.only = TRUE)
+# import packages
+library(tidyverse)
 
-#import degradation data from MSFragger
-#HEK_Nt_1
+### import degradation data from MSFragger
+## Nterm
+# HEK_Nt_1
 HEK_Nt_1_psm <- read_tsv(
   'data_source/raw_data/HEK_Nt_1_psm.tsv',
   col_names = TRUE,
@@ -23,7 +23,7 @@ HEK_Nt_1_psm <- read_tsv(
 
 write_csv(HEK_Nt_1_psm, file = 'data_source/psm/HEK_Nt_1_psm.csv')
 
-#HEK_Nt_2
+# HEK_Nt_2
 HEK_Nt_2_psm <- read_tsv(
   'data_source/raw_data/HEK_Nt_2_psm.tsv',
   col_names = TRUE,
@@ -43,7 +43,7 @@ HEK_Nt_2_psm <- read_tsv(
 
 write_csv(HEK_Nt_2_psm, file = 'data_source/psm/HEK_Nt_2_psm.csv')
 
-#HEK_Nt_3
+# HEK_Nt_3
 HEK_Nt_3_psm <- read_tsv(
   'data_source/raw_data/HEK_Nt_3_psm.tsv',
   col_names = TRUE,
@@ -63,4 +63,63 @@ HEK_Nt_3_psm <- read_tsv(
 
 write_csv(HEK_Nt_3_psm, file = 'data_source/psm/HEK_Nt_3_psm.csv')
 
+## Whole Proteome
+# HEK_WP_1
+HEK_WP_1_psm <- read_tsv(
+  'data_source/raw_data/HEK_WP_1_psm.tsv',
+  col_names = TRUE,
+  name_repair = "universal"
+) |> 
+  filter(Is.Unique == TRUE) |> 
+  select(Peptide, Modified.Peptide, Observed.M.Z, Hyperscore, 
+         Protein.Start, Protein.End, Is.Unique,
+         Assigned.Modifications, UniProt_Accession = Protein.ID, Gene, Entry.Name,
+         'deg_126_0h' = ..126_0h, 'deg_127_3h' = ..127_3h, 'deg_128_6h' = ..128_6h,
+         'deg_129_9h' = ..129_9h, 'deg_130_12h' = ..130_12h, 'deg_131_24h' = ..131_24h) |> 
+  filter(str_detect(Entry.Name, 'HUMAN')) |> 
+  mutate(
+    Index = str_c(UniProt_Accession, Protein.Start, sep = '_')
+  ) |> 
+  select(Index, everything())
 
+write_csv(HEK_WP_1_psm, file = 'data_source/psm/HEK_WP_1_psm.csv')
+
+# HEK_WP_2
+HEK_WP_2_psm <- read_tsv(
+  'data_source/raw_data/HEK_WP_2_psm.tsv',
+  col_names = TRUE,
+  name_repair = "universal"
+) |> 
+  filter(Is.Unique == TRUE) |> 
+  select(Peptide, Modified.Peptide, Observed.M.Z, Hyperscore, 
+         Protein.Start, Protein.End, Is.Unique,
+         Assigned.Modifications, UniProt_Accession = Protein.ID, Gene, Entry.Name,
+         'deg_126_0h' = ..126_0h, 'deg_127_3h' = ..127_3h, 'deg_128_6h' = ..128_6h,
+         'deg_129_9h' = ..129_9h, 'deg_130_12h' = ..130_12h, 'deg_131_24h' = ..131_24h) |> 
+  filter(str_detect(Entry.Name, 'HUMAN')) |> 
+  mutate(
+    Index = str_c(UniProt_Accession, Protein.Start, sep = '_')
+  ) |> 
+  select(Index, everything())
+
+write_csv(HEK_WP_2_psm, file = 'data_source/psm/HEK_WP_2_psm.csv')
+
+# HEK_WP_3
+HEK_WP_3_psm <- read_tsv(
+  'data_source/raw_data/HEK_WP_3_psm.tsv',
+  col_names = TRUE,
+  name_repair = "universal"
+) |> 
+  filter(Is.Unique == TRUE) |> 
+  select(Peptide, Modified.Peptide, Observed.M.Z, Hyperscore, 
+         Protein.Start, Protein.End, Is.Unique,
+         Assigned.Modifications, UniProt_Accession = Protein.ID, Gene, Entry.Name,
+         'deg_126_0h' = ..126_0h, 'deg_127_3h' = ..127_3h, 'deg_128_6h' = ..128_6h,
+         'deg_129_9h' = ..129_9h, 'deg_130_12h' = ..130_12h, 'deg_131_24h' = ..131_24h) |> 
+  filter(str_detect(Entry.Name, 'HUMAN')) |> 
+  mutate(
+    Index = str_c(UniProt_Accession, Protein.Start, sep = '_')
+  ) |> 
+  select(Index, everything())
+
+write_csv(HEK_WP_3_psm, file = 'data_source/psm/HEK_WP_3_psm.csv')
